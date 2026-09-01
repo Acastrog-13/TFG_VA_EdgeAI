bede1764e6b69050ca7e94d8f1177a270e0304ac
Acastrog-13 <161310060+Acastrog-13@users.noreply.github.com>
2026-09-01 10:31:36 +0200

    (firmware-sensor): se habilitan GPIO para leds de salida y botón

--- firmware-sensor/Core/Src/main.c
+++ firmware-sensor/Core/Src/main.c
@@ -389,19 +389,29 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_14, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, led_rojo_Pin|led_amarillo_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, led_verde_Pin|led_azul_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOG, RST_Pin|PWDN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  /*Configure GPIO pin : p_on_Pin */
+  GPIO_InitStruct.Pin = p_on_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(p_on_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB0 PB14 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_14;
+  /*Configure GPIO pins : led_rojo_Pin led_amarillo_Pin */
+  GPIO_InitStruct.Pin = led_rojo_Pin|led_amarillo_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : led_verde_Pin led_azul_Pin */
+  GPIO_InitStruct.Pin = led_verde_Pin|led_azul_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -413,6 +423,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(p_on_EXTI_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(p_on_EXTI_IRQn);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
